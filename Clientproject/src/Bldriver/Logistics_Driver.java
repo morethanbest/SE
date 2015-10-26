32e0ca0a5572bd15caf87abc8d13b4dc727471d8
schao <18260038817@163.com>
2015-10-26 08:43:27 +0800

    bl驱动

---
+++ Clientproject/src/Bldriver/Logistics_Driver.java
@@ -0,0 +1,27 @@
+package Bldriver;
+
+import java.rmi.RemoteException;
+
+import po.Ordertype;
+import vo.OrderVO;
+import businesslogic.logisticsbl.GetOrderbyONBlService_Stub;
+import businesslogicservice.logisticsblservice.GetOrderbyONBlService;
+
+public class Logistics_Driver {
+    public void drive(GetOrderbyONBlService getOrderbyON) throws RemoteException{
+    	OrderVO vo=getOrderbyON.getOrderbyON(1);
+    	if(vo!=null){
+    		System.out.println("Search succeeds");
+    	}
+    	else{
+    		System.out.println("Search fails");
+    	}
+    }
+	public static void main(String[] args) throws RemoteException {
+		// TODO Auto-generated method stub
+		GetOrderbyONBlService getOrderbyON=new GetOrderbyONBlService_Stub();
+		Logistics_Driver driver=new Logistics_Driver();
+		driver.drive(getOrderbyON);
+	}
+
+}
---
+++ Clientproject/src/Bldriver/Orders_Driver.java
@@ -0,0 +1,48 @@
+package Bldriver;
+
+import java.rmi.RemoteException;
+
+import po.Ordertype;
+import vo.OrderFareVO;
+import vo.OrderVO;
+import vo.ReceptionVO;
+import vo.ResultMessage;
+import businesslogic.orderbl.OrderBlService_Stub;
+import businesslogicservice.orderblservice.OrderBlService;
+
+public class Orders_Driver {
+	 public void drive(OrderBlService orderBlService) throws RemoteException{
+		    OrderVO vo=new OrderVO("1","1","1","1","1","1","1","1","1","1",1,1,1,"1",1,1,"1",Ordertype.economical);
+	    	ResultMessage result=orderBlService.order(vo);
+	    	if(result==ResultMessage.success){
+	    		System.out.println("order succeeds");
+	    	}
+	    	else{
+	    		System.out.println("order fails");
+	    	}
+	    	OrderFareVO vo1=new OrderFareVO(null, null, 0, 0, 0, 0, 0, null);
+	    	double result1=orderBlService.order(vo1);
+	    	if(result1==0){
+	    		System.out.println("orderfare succeeds");
+	    	}
+	    	else{
+	    		System.out.println("orderfare fails");
+	    	}
+	    	ReceptionVO vo2=new ReceptionVO(null, null, 0);
+	    	ResultMessage result2=orderBlService.reception(vo2);
+	    	if(result==ResultMessage.success){
+	    		System.out.println("reception succeeds");
+	    	}
+	    	else{
+	    		System.out.println("reception fails");
+	    	}
+	    }
+		public static void main(String[] args) throws RemoteException {
+			// TODO Auto-generated method stub
+			OrderBlService orderBlService=new OrderBlService_Stub();
+			Orders_Driver driver=new Orders_Driver();
+			driver.drive(orderBlService);
+		}
+
+
+}
