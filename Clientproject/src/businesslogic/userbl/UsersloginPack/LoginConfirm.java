bff1cd9f4d70e2d5d1f361203cacf736253451e7
sk <675355574@qq.com>
2015-11-19 23:26:07 +0800

    好了

--- Clientproject/src/businesslogic/userbl/UsersloginPack/LoginConfirm.java
+++ Clientproject/src/businesslogic/userbl/UsersloginPack/LoginConfirm.java
@@ -1,5 +1,7 @@
 package businesslogic.userbl.UsersloginPack;
 
+import java.rmi.RemoteException;
+
 import vo.ResultMessage;
 import data.datafactory.databaseFactory;
 import data.userdata.UserLoginData;
@@ -10,7 +12,13 @@ public class LoginConfirm {
 		databaseFactory fac=new databaseFactory();
 		UserLoginData userdata=fac.getUserLogin();
 		String idstr=Long.toString(id);
-		ResultMessage result=userdata.login(idstr,password);
+		try {
+			ResultMessage result=userdata.login(idstr,password);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			System.out.println("RemoteException!!");
+			e.printStackTrace();
+		}
 		return ResultMessage.success;
 	}
 
