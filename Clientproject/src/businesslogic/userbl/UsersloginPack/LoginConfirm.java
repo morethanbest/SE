58af7a7b22bcaa12f77502582c6e1802ed0e6d89
s402621342 <song402621342@126.com>
2015-11-20 23:42:28 +0800

    update login logic

--- Clientproject/src/businesslogic/userbl/UsersloginPack/LoginConfirm.java
+++ Clientproject/src/businesslogic/userbl/UsersloginPack/LoginConfirm.java
@@ -5,16 +5,21 @@ import java.rmi.RemoteException;
 import data.datafactory.databaseFactory;
 import data.userdata.UserLoginData;
 import po.Job;
+import po.LoginPO;
+import vo.LoginVO;
 
 public class LoginConfirm {
 
-	public Job log(long id, String password){
+	public LoginVO log(long id, String password){
 		databaseFactory fac=new databaseFactory();
 		UserLoginData userdata=fac.getUserLogin();
+	
 		try {
-			return userdata.login(id,password);
+			LoginPO po=userdata.login(id, password);
+			LoginVO vo=new LoginVO(po.getUsername(),po.getHall(),po.getJob());
+			return vo;
 		} catch (RemoteException e) {
-			return Job.visitor;
+			return new LoginVO(null,null,Job.visitor);
 		}
 		
 	}
