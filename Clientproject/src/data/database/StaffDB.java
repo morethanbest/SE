6fcbd5f004a2264f1fc725a530081fdd2f519d47
s402621342 <song402621342@126.com>
2015-11-21 22:07:29 +0800

    staff update

--- Clientproject/src/data/database/StaffDB.java
+++ Clientproject/src/data/database/StaffDB.java
@@ -2,6 +2,8 @@ package data.database;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import po.Job;
 import po.StaffPO;
@@ -27,7 +29,7 @@ public class StaffDB {
 			if (result == ResultMessage.success) {
 				System.out.println("add Successfully");
 			}
-			StaffPO po1 = search("sunchao");
+			StaffPO po1 = search(1);
 			if (po1!=null) {
 				System.out.println("get it");
 			}
@@ -80,15 +82,15 @@ public class StaffDB {
 		return ResultMessage.failure;
 	}
 	
-	public static ResultMessage update(String originalname,String newname,Job job){
+	public static ResultMessage update(long id,String name,Job job){
 		try{
 			byte[] jobbytes = Serialize.Object2Bytes(job);
 			dbh = new DBHelper();
-			sql = "update StaffPO set name=?,job=? where name=?";
+			sql = "update StaffPO set name=?,job=? where id=?";
 			pst = dbh.prepare(sql);
-			pst.setString(1, newname);
+			pst.setString(1, name);
 			pst.setBytes(2, jobbytes);
-			pst.setString(3, originalname);
+			pst.setLong(3,id);
 			int result;
 			result = pst.executeUpdate();
 			if (result != 0) {
@@ -102,36 +104,92 @@ public class StaffDB {
 		return ResultMessage.failure;
 	}
 	
-//	public static List<ConstantsPO> fuzzySearch(String name){
-//		List<ConstantsPO> list=new ArrayList<ConstantsPO>();
-//		ConstantsPO po;
-//		dbh=new DBHelper();
-//		sql="select id,name,value from ConstantsPO where name like ?";
-//		pst = dbh.prepare(sql);
-//		try {
-//			pst.setString(1,"%"+name+"%");	//模糊查找时两边加%
-//			ret=pst.executeQuery();
-//			while(ret.next()){
-//				po=new ConstantsPO(ret.getLong(1),ret.getString(2),ret.getDouble(3));
-//				list.add(po);
-//			}
-//			ret.close();
-//			dbh.close();// 关闭连接
-//			
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		return list;
-//	}
-	
-	
-	public static StaffPO search(String name){
-		StaffPO po=null;
+	public static List<StaffPO> fuzzySearch(String name){
+		List<StaffPO> list=new ArrayList<StaffPO>();
+		StaffPO po;
 		dbh=new DBHelper();
-		sql="select id,name,job from StaffPO where name = ?";
+		sql="select id,name,job from StaffPO where name like ?";
 		pst = dbh.prepare(sql);
 		try {
-			pst.setString(1,name);	
+			pst.setString(1,"%"+name+"%");	//模糊查找时两边加%
+			ret=pst.executeQuery();
+			while(ret.next()){
+				byte[] jobbytes=ret.getBytes(3);
+				Job job=(Job) Serialize.Bytes2Object(jobbytes);
+				po=new StaffPO(ret.getLong(1),ret.getString(2),job);
+				list.add(po);
+			}
+			ret.close();
+			dbh.close();// 关闭连接
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	public static List<StaffPO> fuzzySearchbyjob(Job jobget) {
+		List<StaffPO> list = new ArrayList<StaffPO>();
+		try {
+			byte[] jobbyte = Serialize.Object2Bytes(jobget);
+
+			StaffPO po;
+			dbh = new DBHelper();
+			sql = "select id,name,job from StaffPO where job=?";
+			pst = dbh.prepare(sql);
+
+			pst.setBytes(1, jobbyte);
+			ret = pst.executeQuery();
+			while (ret.next()) {
+				byte[] jobbytes = ret.getBytes(3);
+				Job job = (Job) Serialize.Bytes2Object(jobbytes);
+				po = new StaffPO(ret.getLong(1), ret.getString(2), job);
+				list.add(po);
+			}
+			ret.close();
+			dbh.close();// 关闭连接
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	public static List<StaffPO> fuzzySearchbyboth(String name, Job jobget) {
+		List<StaffPO> list = new ArrayList<StaffPO>();
+		try {
+			byte[] jobbyte = Serialize.Object2Bytes(jobget);
+
+			StaffPO po;
+			dbh = new DBHelper();
+			sql = "select id,name,job from StaffPO where job=?,name like ?";
+			pst = dbh.prepare(sql);
+
+			pst.setBytes(1, jobbyte);
+			pst.setString(2, "%" + name + "%"); // 模糊查找时两边加%
+			ret = pst.executeQuery();
+			while (ret.next()) {
+				byte[] jobbytes = ret.getBytes(3);
+				Job job = (Job) Serialize.Bytes2Object(jobbytes);
+				po = new StaffPO(ret.getLong(1), ret.getString(2), job);
+				list.add(po);
+			}
+			ret.close();
+			dbh.close();// 关闭连接
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	public static StaffPO search(long id){
+		StaffPO po=null;
+		dbh=new DBHelper();
+		sql="select id,name,job from StaffPO where id = ?";
+		pst = dbh.prepare(sql);
+		try {
+			pst.setLong(1,id);	
 			ret=pst.executeQuery();
 			if(ret.next()){
 				byte[] jobbytes=ret.getBytes(3);
@@ -167,6 +225,7 @@ public class StaffDB {
 	}
 	public static void main(String[] args) {
 		initialize();
+		if((fuzzySearchbyjob(Job.Courier)!=null))
 		System.out.println(getLastId());
 	}
 }
--- Clientproject/src/data/managerdata/StaffData.java
+++ Clientproject/src/data/managerdata/StaffData.java
@@ -1,9 +1,11 @@
 package data.managerdata;
 
 import java.rmi.RemoteException;
+import java.util.List;
 
 import data.database.StaffDB;
 import dataservice.managerdataservice.StaffDataService;
+import po.Job;
 import po.StaffPO;
 import vo.ResultMessage;
 
@@ -24,17 +26,25 @@ public class StaffData implements StaffDataService {
 	}
 
 	@Override
-	public ResultMessage updateStaff(String originalname,StaffPO po) throws RemoteException {
+	public ResultMessage updateStaff(StaffPO po) throws RemoteException {
 		// TODO Auto-generated method stub
-		ResultMessage result=StaffDB.update(originalname, po.getName(), po.getJob());
+		ResultMessage result=StaffDB.update(po.getId(),po.getName(), po.getJob());
 		return result;
 	}
 	
+
 	@Override
-	public StaffPO findStaff(String name) throws RemoteException {
+	public StaffPO findStaff(long id) throws RemoteException {
 		// TODO Auto-generated method stub
-		StaffPO po=StaffDB.search(name);
+		StaffPO po=StaffDB.search(id);
 		return po;
+	}
+
+	@Override
+	public List<StaffPO> fuzzyfindStaff(String name) throws RemoteException {
+		// TODO Auto-generated method stub
+		List<StaffPO> list=StaffDB.fuzzySearch(name);
+		return list;
 	}
 
 	@Override
@@ -42,6 +52,20 @@ public class StaffData implements StaffDataService {
 		// TODO Auto-generated method stub
 		long id=StaffDB.getLastId();
 		return id;
+	}
+
+	@Override
+	public List<StaffPO> fuzzyfindbyjob(Job job) throws RemoteException {
+		// TODO Auto-generated method stub
+		List<StaffPO> list=StaffDB.fuzzySearchbyjob(job);
+		return list;
+	}
+
+	@Override
+	public List<StaffPO> fuzzyfindbyboth(String name, Job job) throws RemoteException {
+		// TODO Auto-generated method stub
+		List<StaffPO> list=StaffDB.fuzzySearchbyboth(name, job);
+		return list;
 	}
 
 
