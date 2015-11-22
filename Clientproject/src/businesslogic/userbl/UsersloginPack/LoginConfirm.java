41cefb92d2dcfc24fdc686e013a4578fedd515d3
s402621342 <song402621342@126.com>
2015-11-22 21:11:29 +0800

    你慢慢试

--- Clientproject/src/businesslogic/userbl/UsersloginPack/LoginConfirm.java
+++ Clientproject/src/businesslogic/userbl/UsersloginPack/LoginConfirm.java
@@ -2,20 +2,24 @@ package businesslogic.userbl.UsersloginPack;
 
 import java.rmi.RemoteException;
 
-import RMI.FactoryGet;
-import data.datafactory.databaseFactory;
 import data.userdata.UserLoginData;
+import dataservice.userdataservice.UserLoginDataService;
 import po.Job;
+import po.LoginPO;
+import vo.LoginVO;
 
 public class LoginConfirm {
 
-	public Job log(long id, String password){
-		databaseFactory fac=FactoryGet.getfactory();
-		UserLoginData userdata=fac.getUserLogin();
+	public LoginVO log(long id, String password){
+//		databaseFactory fac=FactoryGet.getfactory();
+//		UserLoginData userdata=fac.getUserLogin();
+		UserLoginDataService userdata=new UserLoginData();
 		try {
-			return userdata.login(id,password);
+			LoginPO po=userdata.login(id,password);
+			
+			return new LoginVO(po.getUsername(),po.getHall(),po.getJob());
 		} catch (RemoteException e) {
-			return Job.visitor;
+			return new LoginVO(null,null,Job.visitor);
 		}
 		
 	}
