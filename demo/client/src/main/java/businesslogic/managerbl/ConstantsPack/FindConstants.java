8b7ee3cc85ebca1d6deb63e1d85176ea882a6672
schao <18260038817@163.com>
2015-11-24 10:57:33 +0800

    删除无用mock和消除查找常量时的重复

--- demo/client/src/main/java/businesslogic/managerbl/ConstantsPack/FindConstants.java
+++ demo/client/src/main/java/businesslogic/managerbl/ConstantsPack/FindConstants.java
@@ -3,7 +3,6 @@ package businesslogic.managerbl.ConstantsPack;
 import java.util.ArrayList;
 import java.util.List;
 
-import data.managerdata.ConstantsData;
 import dataservice.managerdataservice.ConstantsDataService;
 import init.RMIHelper;
 import po.ConstantsPO;
@@ -55,7 +54,26 @@ public class FindConstants {
 			listvo=null;
 			System.out.println("fuzzyfindconstants fail");
 		}
-		
+		if(name.equals("距离")){
+			for(int i=0;i<listvo.size();i++){
+				ConstantsVO vo1=listvo.get(i);
+				String[] splits1=vo1.getName().split("-");
+				String beforeadd1=splits1[1];
+				String afteradd1=splits1[2];
+				for(int j=0;j<i;j++){
+					ConstantsVO vo2=listvo.get(j);
+					String[] splits2=vo2.getName().split("-");
+					String beforeadd2=splits2[1];
+					String afteradd2=splits2[2];
+					if(beforeadd1.equals(afteradd2)&&afteradd1.equals(beforeadd2)){
+						listvo.remove(i);
+						i--;
+						break;
+					}
+				}
+
+			}
+		}
 		return listvo;
 		
 	}
