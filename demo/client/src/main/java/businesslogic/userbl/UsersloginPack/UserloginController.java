2cedbcd73d114c825ece8b8359744453f3547f4e
schao <18260038817@163.com>
2015-11-26 22:53:01 +0800

    用户登录修改

--- demo/client/src/main/java/businesslogic/userbl/UsersloginPack/UserloginController.java
+++ demo/client/src/main/java/businesslogic/userbl/UsersloginPack/UserloginController.java
@@ -10,10 +10,10 @@ public class UserloginController implements UserloginBlService{
 
 
 	@Override
-	public LoginVO login(long id, String password) {
+	public LoginVO login(String username, String password) {
 		LoginConfirm con=new LoginConfirm();
 		Userlogin login=new Userlogin(con);
-		LoginVO vo=login.login(id,password);
+		LoginVO vo=login.login(username,password);
 		return vo;
 	}
 }
