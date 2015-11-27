9c439e4a5f07b02dd5d011e80e75d8806930c875
sk <675355574@qq.com>
2015-11-27 19:12:44 +0800

    库存分区调整的注释

--- demo/client/src/main/java/businesslogicservice/commodityblservice/SubregionBlService.java
+++ demo/client/src/main/java/businesslogicservice/commodityblservice/SubregionBlService.java
@@ -8,3 +8,7 @@ import po.ResultMessage;
 public interface SubregionBlService {
 	public ResultMessage Subregion(CommodityLocation beforeLocation,CommodityLocation afterLocation)throws RemoteException;
 }
+
+
+     //可以考虑根据区号来进行调整 就是说 可以从一个区 移动 可选个数量的库存到另外一个区内
+	 //   参数为  blocknum1   blocknum2   number（移动的库存的数量）
