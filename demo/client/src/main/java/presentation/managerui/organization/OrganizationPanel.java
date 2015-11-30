d303e7336125dbc2790fab6cf6e42436cd3350e1
bigdream <609402897@qq.com>
2015-11-30 15:30:40 +0800

    Revert "可以测试"
    This reverts commit 62cf00c703e53c4ec9e821ec38576917adb8f4a4.

--- demo/client/src/main/java/presentation/managerui/organization/OrganizationPanel.java
+++ demo/client/src/main/java/presentation/managerui/organization/OrganizationPanel.java
@@ -18,82 +18,90 @@ import javax.swing.table.DefaultTableModel;
 import po.Organizationtype;
 import presentation.enums.OrganizationType;
 import vo.CityVO;
-import vo.ConstantsVO;
 import vo.OrganizationVO;
 import businesslogic.managerbl.ConstantsPack.ConstantsController;
 import businesslogic.managerbl.OrganizationPack.OrganizationController;
 import businesslogicservice.managerblservice.ConstantsBlService;
 import businesslogicservice.managerblservice.OrganizationBlService;
 
-public class OrganizationPanel extends JPanel implements ActionListener {
+public class OrganizationPanel extends JPanel {
 	private JTable table;
 	private JComboBox<String> orgSelect;
 	private JComboBox<String> citySelect;
 	private JButton btnAdd;
-	private JButton btnDelete;
-	private JButton btnRevise;
+	private JButton btnNewButton;
+	private JButton btnNewButton_1;
 	private List<OrganizationVO> list;
 	private OrganizationBlService organizationBlService;
-
 	/**
 	 * Create the panel.
 	 */
 	public OrganizationPanel() {
 		setLayout(null);
-
+		
 		organizationBlService = new OrganizationController();
-
+		
 		JSeparator separator = new JSeparator();
 		separator.setBounds(0, 58, 954, 10);
 		add(separator);
-
+		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 81, 930, 326);
 		add(scrollPane);
-
+		
 		table = new JTable();
 		scrollPane.setViewportView(table);
-
+		
 		orgSelect = new JComboBox<String>();
 		orgSelect.setBounds(0, 0, 174, 31);
 		add(orgSelect);
 		addOrgTypeItems();
-
+		
 		citySelect = new JComboBox<String>();
 		citySelect.setBounds(199, 0, 174, 31);
 		add(citySelect);
 		addCityItems();
-
+		
 		ItemListener listener = new ItemListener() {
-
+			
 			@Override
 			public void itemStateChanged(ItemEvent e) {
 				refreshList();
+				
 			}
 		};
-
+		
 		citySelect.addItemListener(listener);
 		orgSelect.addItemListener(listener);
-
+		
 		btnAdd = new JButton("Add");
-		btnAdd.addActionListener(this);
+		btnAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+					AddOrganizationDailog dialog = new AddOrganizationDailog();
+					dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+					dialog.setVisible(true);
+			}
+		});
 		btnAdd.setBounds(587, 0, 113, 31);
 		add(btnAdd);
-
-		btnDelete = new JButton("Delete");
-		btnDelete.addActionListener(this);
-		btnDelete.setBounds(714, 0, 113, 31);
-		add(btnDelete);
-
-		btnRevise = new JButton("Revise");
-		btnRevise.setBounds(841, 0, 113, 31);
-		add(btnRevise);
-
+		
+		btnNewButton = new JButton("Delete");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(714, 0, 113, 31);
+		add(btnNewButton);
+		
+		btnNewButton_1 = new JButton("Revise");
+		btnNewButton_1.setBounds(841, 0, 113, 31);
+		add(btnNewButton_1);
+		
 		DefaultTableModel tableModel = (DefaultTableModel) table.getModel();
 		tableModel.setColumnCount(4);
 		tableModel.setRowCount(10);
 	}
-
+	
 	public void addCityItems() {
 		citySelect.removeAllItems();
 		ConstantsBlService constantsBlService = new ConstantsController();
@@ -103,14 +111,14 @@ public class OrganizationPanel extends JPanel implements ActionListener {
 			citySelect.addItem(city.getName());
 		}
 	}
-
+	
 	private void addOrgTypeItems() {
 		orgSelect.addItem("全部");
 		for (OrganizationType org : OrganizationType.values()) {
 			orgSelect.addItem(org.getName());
 		}
 	}
-
+	
 	private void displayByVO() {
 		DefaultTableModel tableModel = (DefaultTableModel) table.getModel();
 		tableModel.setRowCount(0);// 清除原有行
@@ -133,79 +141,41 @@ public class OrganizationPanel extends JPanel implements ActionListener {
 		}
 
 	}
-
-	void refreshList() {
-		if (citySelect.getSelectedItem() == null
-				|| orgSelect.getSelectedItem() == null)
+	
+	void refreshList(){
+		if(citySelect.getSelectedItem() == null || orgSelect.getSelectedItem() == null)
 			return;
-		if (citySelect.getSelectedItem().equals("全部")
+		if(citySelect.getSelectedItem().equals("全部") 
 				&& orgSelect.getSelectedItem().equals("全部"))
 			list = organizationBlService.getOrganizationAll();
-		else if (citySelect.getSelectedItem().equals("全部"))
-			list = organizationBlService
-					.getOrganizationbyType(getOrganizationType((String) orgSelect
-							.getSelectedItem()));
-		else if (orgSelect.getSelectedItem().equals("全部"))
-			list = organizationBlService
-					.getOrganizationbyCity((String) citySelect
-							.getSelectedItem());
+		else if(citySelect.getSelectedItem().equals("全部"))
+			list = organizationBlService.getOrganizationbyType(getOrganizationType((String) orgSelect.getSelectedItem()));
+		else if(orgSelect.getSelectedItem().equals("全部"))
+			list = organizationBlService.getOrganizationbyCity((String) citySelect.getSelectedItem());
 		else
-			list = organizationBlService.getOrganizationbyBoth(
-					(String) citySelect.getSelectedItem(),
-					getOrganizationType((String) orgSelect.getSelectedItem()));
-
+			list = organizationBlService.getOrganizationbyBoth((String) citySelect.getSelectedItem(), getOrganizationType((String) orgSelect.getSelectedItem()));
+		
 		displayByVO();
 	}
-
-	private Organizationtype getOrganizationType(String str) {
+	private Organizationtype getOrganizationType(String str){
 		for (OrganizationType org : OrganizationType.values()) {
-			if (org.getName().equals(str)) {
+			if(org.getName().equals(str)){
 				return org.getOrg();
 			}
 		}
-
+		
 		return null;
 	}
+	
 
-	private String getOrganizationStr(Organizationtype org) {
+	private String getOrganizationStr(Organizationtype org){
 		for (OrganizationType orgs : OrganizationType.values()) {
-			if (orgs.getOrg() == org) {
+			if(orgs.getOrg() == org){
 				return orgs.getName();
 			}
 		}
-
+		
 		return null;
 	}
-
-	void addOrganization(String name, String code, String org, String city){
-		organizationBlService.addOrganization(new OrganizationVO(name, code, getOrganizationType(org), city));
-	}
 	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource().equals(btnAdd)) {
-			AddOrganizationDailog dialog = new AddOrganizationDailog(this);
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-		} else if(e.getSource().equals(btnDelete)){
-			DefaultTableModel tableModel = (DefaultTableModel) table
-					.getModel();
-			try {
-				String cellValue1 = (String) tableModel.getValueAt(
-						table.getSelectedRow(), 0);
-				String cellValue2 = (String) tableModel.getValueAt(
-						table.getSelectedRow(), 1);
-				String cellValue3 = (String) tableModel.getValueAt(
-						table.getSelectedRow(), 2);
-				String cellValue4 = (String) tableModel.getValueAt(
-						table.getSelectedRow(), 3);
-				organizationBlService.delOrganization(new OrganizationVO(cellValue2, cellValue1, getOrganizationType(cellValue4), cellValue3));
-				refreshList();
-			} catch (ArrayIndexOutOfBoundsException | NullPointerException e1) {
-//				managerPanel.setHint("系统提示：未选择删除项！");
-			}
-		}
-
-	}
-
 }
