c694297f28a0eeb02caba72cd8341795c0b6033a
s402621342 <song402621342@126.com>
2015-12-03 13:00:02 +0800

    ip change

--- demo/server/src/main/java/data/database/DBHelper.java
+++ demo/server/src/main/java/data/database/DBHelper.java
@@ -6,7 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
   
 public class DBHelper {  
-    private static final String url = "jdbc:mysql://114.212.43.239/delivery?"+ "useUnicode=true&characterEncoding=UTF8";  
+	//寝室的ip为172.26.15.15
+    private static final String url = "jdbc:mysql://172.26.15.15/delivery?"+ "useUnicode=true&characterEncoding=UTF8";  
     private static final String name = "com.mysql.jdbc.Driver";  
     private static final String user = "sunchao";  
     private static final String password = "123456";  
