6b03140f78649e81d0804dc50a78bb1360cdf515
sk <675355574@qq.com>
2015-12-03 20:18:35 +0800

    recordpay

--- demo/client/src/main/java/businesslogic/balancebl/RecordpayPack/FindRecord.java
+++ demo/client/src/main/java/businesslogic/balancebl/RecordpayPack/FindRecord.java
@@ -1,8 +1,11 @@
 package businesslogic.balancebl.RecordpayPack;
 
+import dataservice.balancedataservice.RecordpayFormDataService;
+import init.RMIHelper;
 import po.Formstate;
 import po.RecordpayPO;
 
+import java.rmi.RemoteException;
 import java.util.List;
 
 /**
@@ -10,6 +13,14 @@ import java.util.List;
  */
 public class FindRecord {
     public List<RecordpayPO> findforms(Formstate formstate){
-
+        List<RecordpayPO> pos=null;
+        RecordpayFormDataService data= RMIHelper.getRecordpay();
+        try {
+            pos=data.getRecordpayForm(formstate);
+        } catch (RemoteException e) {
+            System.out.println("record pay find forms by formstate!!!!");
+            e.printStackTrace();
+        }
+        return pos;
     }
 }
--- demo/client/src/main/java/businesslogic/balancebl/RecordpayPack/Recordpay.java
+++ demo/client/src/main/java/businesslogic/balancebl/RecordpayPack/Recordpay.java
@@ -1,28 +1,56 @@
 package businesslogic.balancebl.RecordpayPack;
 
+import po.Formstate;
+import po.RecordpayList;
+import po.RecordpayPO;
+import po.ResultMessage;
 import vo.RecordpayVO;
 import vo.ResultMessage;
 
+import javax.xml.transform.Result;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Recordpay {
-	MockIdInfo idInfo;
-	MockRecord record;
-	public Recordpay(MockIdInfo idInfo,MockRecord record){
-		this.idInfo=idInfo;
-		this.record=record;
-	}
-	public ResultMessage Recordpay(RecordpayVO vo){
-		long id=idInfo.getid();
-		long paytime=vo.getPaytime();
+	AddRecord addRecord;
+	UpdateRecord update;
+	FindRecord find;
+	public ResultMessage add(RecordpayVO vo){
+		String id=vo.getId();
+		long paytime=vo.getPaytime()
 		double paysum=vo.getPaysum();
 		String payman=vo.getPayman();
 		String payaccount=vo.getPayaccount();
-		double rent=vo.getRent();
-		double fee=vo.getFee();
-		double salary=vo.getSalary();
-		double reward=vo.getReward();
+		RecordpayList entry=vo.getEntry();
 		String remark=vo.getRemark();
-		record.setRecordInfo(id, paytime, paysum, payman, payaccount, rent, fee, salary, reward, remark);
-		ResultMessage result=record.Recordpay();
+		Formstate formstate=vo.getFormstate();
+
+		addRecord.set(id,paytime,paysum,payman,payaccount,entry,remark,formstate);
+		ResultMessage result=addRecord.passadd();
 		return result;
+	}
+
+	public ResultMessage update(RecordpayVO vo){
+		String id=vo.getId();
+		long paytime=vo.getPaytime()
+		double paysum=vo.getPaysum();
+		String payman=vo.getPayman();
+		String payaccount=vo.getPayaccount();
+		RecordpayList entry=vo.getEntry();
+		String remark=vo.getRemark();
+		Formstate formstate=vo.getFormstate();
+
+		update.set(id, paytime, paysum, payman, payaccount, entry, remark, formstate);
+		ResultMessage result=update.passupdate();
+		return result;
+	}
+
+	public List<RecordpayVO> find(Formstate formstate){
+		List<RecordpayPO> pos=find.findforms(formstate);
+		List<RecordpayVO> list=new ArrayList<RecordpayVO>();
+		for
+
+
+
 	}
 }
