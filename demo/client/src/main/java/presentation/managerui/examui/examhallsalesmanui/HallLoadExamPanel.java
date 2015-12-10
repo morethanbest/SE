6e50e06f05b719d0d810b4a68eaadf837e8ab64b
bigdream <609402897@qq.com>
2015-12-10 23:27:03 +0800

    新几个审批

---
+++ demo/client/src/main/java/presentation/managerui/examui/examhallsalesmanui/HallLoadExamPanel.java
@@ -0,0 +1,14 @@
+package presentation.managerui.examui.examhallsalesmanui;
+
+import javax.swing.JPanel;
+
+public class HallLoadExamPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public HallLoadExamPanel() {
+
+	}
+
+}
