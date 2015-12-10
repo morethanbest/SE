6939f15d58b88c823155d6ed66f8f2b35cec2834
schao <18260038817@163.com>
2015-12-10 20:25:50 +0800

    付款单审批

--- demo/client/src/main/java/presentation/managerui/examui/ExamPanel.java
+++ demo/client/src/main/java/presentation/managerui/examui/ExamPanel.java
@@ -9,6 +9,7 @@ import presentation.managerui.examui.examcentersalesmanui.ArrivalExamPanel;
 import presentation.managerui.examui.examcentersalesmanui.CenterLoadExamPanel;
 import presentation.managerui.examui.examcentersalesmanui.TransferExamPanel;
 import presentation.managerui.examui.examcourierui.OrderExamPanel;
+import presentation.managerui.examui.examfinancial.RecordpayExamPanel;
 
 public class ExamPanel extends JPanel {
 
@@ -26,13 +27,14 @@ public class ExamPanel extends JPanel {
 		TransferExamPanel trans = new TransferExamPanel(this, card);
 		CenterLoadExamPanel cl = new CenterLoadExamPanel(this, card);
 		OrderExamPanel order = new OrderExamPanel(this, card);
-		
+		RecordpayExamPanel pay= new RecordpayExamPanel(this, card);
 		
 		add(select, "select");
 		add(arrival, "arrival");
 		add(trans, "trans");
 		add(cl, "cl");
 		add(order, "order");
+		add(pay, "pay");
 	}
 
 }
