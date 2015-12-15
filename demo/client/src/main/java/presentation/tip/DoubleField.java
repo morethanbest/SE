17fe22fc6c771364ad135bc7e9e88797d923cb9d
bigdream <609402897@qq.com>
2015-12-15 22:11:41 +0800

    double的修改

--- demo/client/src/main/java/presentation/tip/DoubleField.java
+++ demo/client/src/main/java/presentation/tip/DoubleField.java
@@ -13,12 +13,17 @@ public class DoubleField extends JTextField {
      }
   
      protected Document createDefaultModel() {
-         return new NumberDocument();
+         return new NumberDocument(this);
      }
   
      static class NumberDocument extends PlainDocument {
+    	 DoubleField doubleField;
   
-         public void insertString(int offs, String str, AttributeSet a) 
+         public NumberDocument(DoubleField doubleField) {
+			this.doubleField = doubleField;
+		}
+
+		public void insertString(int offs, String str, AttributeSet a) 
              throws BadLocationException {
              if (str == null) {
                  return;
@@ -26,9 +31,13 @@ public class DoubleField extends JTextField {
              char[] upper = str.toCharArray();
              char[] temp = new char[upper.length];
              for (int i = 0; i < upper.length; i++) {
-                if((upper[i] >= '0' && upper[i] <= '9')||upper[i]=='.') {
+                if((upper[i] >= '0' && upper[i] <= '9')) {
                     temp[i] = upper[i];
+                } else if(upper[i] == '.'){
+                	if(!doubleField.getText().contains("."))
+                		temp[i] = upper[i];
                 }
+                	
              }
              super.insertString(offs, new String(temp).trim(), a);
          }
